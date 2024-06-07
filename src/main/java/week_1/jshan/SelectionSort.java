cb76892d599f5123ad832f65f843251b5b3d7c59
Henry <dogfootmaster@gmail.com>
2024-06-07 16:23:11 +0900

    선택 정렬 추가.

---
+++ src/main/java/week_1/jshan/SelectionSort.java
@@ -0,0 +1,33 @@
+package week_1.jshan;
+
+
+public class SelectionSort {
+  public static void main(String[] args) {
+
+    int[] nArr = {9,10,2,4,5,1};
+    sortBySelection(nArr);
+    for (int i=0; i<nArr.length; i++) {
+      System.out.print(nArr[i] + " ");
+    }
+  }
+
+  public static void sortBySelection(int[] arr) {
+    int length = arr.length;
+
+    for (int i=0; i<length-1; i++) {
+      int minIdx = i;
+      // 0번 idx 부터 하나씩 채워 간다.
+      // 가장 작은 idx를 루프를 돌면서 찾는다.
+      for (int j=i+1; j<length; j++) {
+        if (arr[minIdx] > arr[j]) {
+          minIdx = j;
+
+        }
+      }
+      // 치환
+      int temp = arr[i];
+      arr[i] = arr[minIdx];
+      arr[minIdx] = temp;
+    }
+  }
+}
