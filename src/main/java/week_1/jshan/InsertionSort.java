8fbf06b627be3163faae68bd234d83b9b16cd604
Henry <dogfootmaster@gmail.com>
2024-06-15 18:41:35 +0900

    주석 및 print 로직 추가.

--- src/main/java/week_1/jshan/InsertionSort.java
+++ src/main/java/week_1/jshan/InsertionSort.java
@@ -1,7 +1,6 @@
 package week_1.jshan;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class InsertionSort {
   public static void main(String[] args) {
@@ -15,13 +14,13 @@ public class InsertionSort {
   public static void sortBySelection(int[] arr) {
     for (int i=1; i<arr.length; i++) {
       int key = arr[i];
-      // System.out.println("key : " + key);
+       System.out.println("key : " + key);
       for (int j=i-1; j>=0; j--) {
         if (key < arr[j]) {
           // 값 치환
           arr[j+1] = arr[j];
           arr[j] = key;
-          // System.out.printf("j[%d] : %d, j[%d+1] : %d \n", j, arr[j], j, arr[j+1]);
+           System.out.printf("j[%d] : %d, j[%d+1] : %d \n", j, arr[j], j, arr[j+1]);
         }
       }
     }
